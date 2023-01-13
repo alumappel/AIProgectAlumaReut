66c7f9d93637f7e7764f123e79c8f666c09cbdd7
alumappel <alumappel@gmail.com>
2023-01-13 17:42:40 +0200

    monotomy model stopp

--- js/myMonotonyScript.js
+++ js/myMonotonyScript.js
@@ -5,6 +5,7 @@
 const URL = "https://teachablemachine.withgoogle.com/models/ZnR7rgHeL/";
 
 
+
 async function createModel() {
     const checkpointURL = URL + "model.json"; // model topology
     const metadataURL = URL + "metadata.json"; // model metadata
@@ -21,14 +22,22 @@ async function createModel() {
     return recognizer;
 }
 
+
+//משתנים לחישוב
 let tempArryM = new Array();
 let staticTempArryM = new Array();
 let monotonyTenSecAVGArry = new Array();
 let VarietyTenSecAVGArry = new Array();
 
 
+// עצירה
+let isListening = true;
+let listenIntervalId;
+
+
+let recognizer;
 async function initMonotony() {
-    const recognizer = await createModel();
+    recognizer = await createModel();
     const classLabels = recognizer.wordLabels(); // get class labels
     const labelContainer = document.getElementById("label-container-monotony");
     for (let i = 0; i < classLabels.length; i++) {
@@ -42,6 +51,7 @@ async function initMonotony() {
     // 1. A callback function that is invoked anytime a word is recognized.
     // 2. A configuration object with adjustable fields
     recognizer.listen(result => {
+        if (!isListening) return;
         const scores = result.scores; // probability of prediction for each class
         // render the probability scores per class
         for (let i = 0; i < classLabels.length; i++) {
@@ -49,10 +59,7 @@ async function initMonotony() {
             labelContainer.childNodes[i].innerHTML = classPrediction;
             //הכנסת התחזית למערך זמני
             tempArryM.push(result.scores[i].toFixed(2));
-            //המספר של כל קלאס במערך
-            // 0 רעש רקע
-            // 1 מונוטוני
-            // 2 מגוון
+            //המספר של כל קלאס במערך            // 0 רעש רקע            // 1 מונוטוני            // 2 מגוון
         }
     }, {
         includeSpectrogram: true, // in case listen should return result.spectrogram
@@ -61,12 +68,8 @@ async function initMonotony() {
         overlapFactor: 0.50 // probably want between 0.5 and 0.75. More info in README
     });
 
-    // Add condition that happens every 10 seconds
-    //כל 10 שניות שמירה במערך זמני יציב
-    //יצירת ממוצעים
-    //עדכון המשוב
-    setInterval(() => {
-        //console.log("This function is called every 10 seconds");
+    // Add condition that happens every 10 seconds    //כל 10 שניות שמירה במערך זמני יציב    //יצירת ממוצעים    //עדכון המשוב
+    listenIntervalId = setInterval(() => {
         staticTempArryM = tempArryM;
         tempArryM = new Array();
         let arrysNamesArry = ["placeHolder", monotonyTenSecAVGArry, VarietyTenSecAVGArry];
@@ -85,17 +88,12 @@ async function initMonotony() {
         }
         updateVisualizationM();
     }, 10000);
-
-
-    // Stop the recognition in 5 seconds.
-    // setTimeout(() => recognizer.stopListening(), 5000);
 }
 
 
 //יצירת משוב מיידי
 async function updateVisualizationM() {
-    //בדוק את החצי דקה האחרונה
-    //אם ___ מעל 0.8 תציג משוב מתאים
+    //בדוק את החצי דקה האחרונה    //אם ___ מעל 0.8 תציג משוב מתאים
     let span = document.getElementById("ansMonTxt");
     const box = document.getElementById("ansMon");
     if (VarietyTenSecAVGArry.length - 1 >= 0.8) {
@@ -109,13 +107,11 @@ async function updateVisualizationM() {
         if (box.classList.contains('feedbackgood') == false) {
             box.classList.add('feedbackgood');
         }
-
     }
     else {
         //הודעה קרוב מדי
         console.log("מונוטוני");
         span.innerHTML = "הדיבור שלך מונוטוני מדי - נסי לגוון";
-
 
         if (box.classList.contains('feedbackgood')) {
             box.classList.remove('feedbackgood');
@@ -124,6 +120,11 @@ async function updateVisualizationM() {
             box.classList.add('feedbackbad');
         }
     }
+}
 
 
+function stopLoopM() {
+    recognizer.stopListening();
+    isListening = false;
+    clearInterval(listenIntervalId);
 }
